2ff743846adb5150a4e136771982b5ece29155ee
decesarojunior <decesarojunior@yahoo.com.br>
2023-08-07 11:40:59 -0300

    projeto inicial da aula de 07/08/2023.

---
+++ src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_20232.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.bcc.too.exercicios.respostas;
+
+/**
+ *
+ * @author telmo
+ */
+public class Java_maven_too_20232 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
