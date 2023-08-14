5d2bcbb46ac7436a1317cced9c4730787a765378
decesarojunior <decesarojunior@yahoo.com.br>
2023-08-14 09:52:32 -0300

    alteracoes da aula de 14/08/2023.

--- src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_20232.java
+++ src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_20232.java
@@ -1,6 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package br.edu.ifsul.bcc.too.exercicios.respostas;
 
@@ -9,6 +7,11 @@ package br.edu.ifsul.bcc.too.exercicios.respostas;
  * @author telmo
  */
 public class Java_maven_too_20232 {
+    
+    
+    
+    
+    
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
